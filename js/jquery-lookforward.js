5a913ffc0bca5df0a5c8ddaaee07153523471804
steelydylan <ess_president@me.com>
2017-09-16 16:20:35 +0900

    [fix] version

--- js/jquery-lookforward.js
+++ js/jquery-lookforward.js
@@ -6,7 +6,7 @@
  *   license: MIT (http://opensource.org/licenses/MIT)
  *   author: appleple
  *   homepage: http://developer.a-blogcms.jp
- *   version: 1.0.0
+ *   version: 1.0.3
  *
  * es6-object-assign:
  *   license: MIT (http://opensource.org/licenses/MIT)
